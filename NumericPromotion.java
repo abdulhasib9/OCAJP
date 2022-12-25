26a6f95da318249bdb957b3f86dcac3ddd58628b
thebeast <yousufzai.abdulhasib@gmail.com>
2022-12-25 15:41:20 -0800

    added more code to the NumericPromotion file

--- NumericPromotion.java
+++ NumericPromotion.java
@@ -12,5 +12,21 @@ public class NumericPromotion{
         //we need to wrap the variables inside the brackets 
         System.out.println("x + y = "+(x+y));
 
+        //example 2
+        byte b =10;
+        int c =5;
+        double d =10.5;
+
+        double result = b+c+d;
+        /*
+         * first b is promoting to the int 
+         * the c is promoting to the double 
+         * no compilation errors because we use larger data types for the result
+         * this is promotion
+         */
+
+        System.out.println("Result = "+result);
+    
+
     }
 }
