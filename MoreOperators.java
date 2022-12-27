5bf1203f2584450fe4aee34766b5497e396c6c82
thebeast <yousufzai.abdulhasib@gmail.com>
2022-12-26 22:51:47 -0800

    added stringBuilderMethods class and examples

---
+++ MoreOperators.java
@@ -0,0 +1,16 @@
+public class MoreOperators {
+    public static void main(String[] args) {
+        int x =2;
+        int y=3;
+        System.out.println("x < y : "+(x<y));
+
+        boolean a =true;
+        boolean b = false;
+        boolean c =true;
+
+        //the ^ operator
+        boolean test1 =false;
+        boolean test2 =false;
+        System.out.println(test1^test2);
+    }
+}
