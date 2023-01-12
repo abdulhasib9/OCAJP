48137ca18841b56272e8c4fd21de804dd0492419
abdulhasib <yousufzai.abdulhasib@gmail.com>
2023-01-11 23:47:38 -0800

    added intellij project files and more code to the ArraysDemo class

--- ArraysDemo.java
+++ ArraysDemo.java
@@ -1,2 +1,17 @@
+import java.util.Arrays;
+
 public class ArraysDemo {
+    public static void main(String[] args) {
+        int [] num = new int[]{23,55,66};
+        for(int i:num)
+            System.out.println(i);
+
+        String[] animals = {"dog","parrot","cat"};
+        String[] myAnimals = animals;
+        String[] otherAnimals ={"dog","parrot","cate"};
+
+        System.out.println(animals.equals(myAnimals));
+        System.out.println(animals==myAnimals);
+        System.out.println(Arrays.equals(animals,otherAnimals));
+    }
 }
