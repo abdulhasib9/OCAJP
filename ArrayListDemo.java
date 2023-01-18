f8ccfecf912cc83204214ef89f8fc893c59d50a4
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-01-17 22:39:40 -0800

    added more examples to the arraylistdemo file

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
@@ -7,5 +8,17 @@ public class ArrayListDemo {
         mylist.add("hasib");
         mylist.add("noman");
         System.out.println(mylist);
+
+        //2nd example
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("cat");
+        list.add("parrot");
+        list.add("dog");
+        list.add(1,"lion"); //this will change parrot to lion
+        list.add("eagle");
+        System.out.println(list);
+
+
     }
 }
