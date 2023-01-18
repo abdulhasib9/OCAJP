9417a746f45aaf87390080a59ae7617b656720b9
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-01-17 22:26:24 -0800

    add ArraylistDemo file and some examples to the file

---
+++ ArrayListDemo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List mylist = new ArrayList();
+        mylist.add("hasib");
+        mylist.add("noman");
+        System.out.println(mylist);
+    }
+}
