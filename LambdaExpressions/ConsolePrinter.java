bbad4fb925399401acb0093f932f29c5357a708e
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-01 21:53:57 -0800

    added Console Printer class and some code examples

---
+++ LambdaExpressions/ConsolePrinter.java
@@ -0,0 +1,8 @@
+package LambdaExpressions;
+
+public class ConsolePrinter implements Printer {
+    @Override
+    public void print (String message){
+        System.out.println(message);
+    }
+}
