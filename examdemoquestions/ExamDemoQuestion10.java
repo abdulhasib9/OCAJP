dba12cfdad27de1984241ab43980a25efee21854
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-13 20:55:10 -0800

    added question one to  examdemoquestion10 class

--- examdemoquestions/ExamDemoQuestion10.java
+++ examdemoquestions/ExamDemoQuestion10.java
@@ -1,4 +1,24 @@
 package examdemoquestions;
 
+import java.io.IOException;
+
 public class ExamDemoQuestion10 {
+    public static void main(String[] args) {
+        //Question 1
+        /*
+        * what will  be the output of question 1\
+        * answer: compilation fails because test function does not throw IO exception
+        *
+        * */
+//        try{
+//            test();
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+    }
+
+    public static void test() {
+        System.out.println("Code does not compile");
+    }
 }
+
